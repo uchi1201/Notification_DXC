4f3f7fbc030ec01a53f3133121de00270d71f353
fanfan <lijuan.fan@dxc.com>
2022-12-08 09:24:45 +0800

    add

---
+++ app/src/main/java/com/android/example/notification/ui/category/CategoryManagementFragment.kt
@@ -0,0 +1,34 @@
+package com.android.example.notification.ui.category
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.android.example.notification.R
+import com.android.example.notification.databinding.FragmentCategoryManagementBinding
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CategoryManagementFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CategoryManagementFragment : Fragment() {
+    private var _binding: FragmentCategoryManagementBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentCategoryManagementBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+        initView()
+        return root
+    }
+
+    private fun initView(){
+
+    }
+
+}
